53d7424d5e246f66fdb51c41bb1e70aab452409c
Ilya Makarevich <>
2023-09-10 21:20:55 +0300

    remove unused code in ServerModels

--- StarWars/ServerModels/Planet.swift
+++ StarWars/ServerModels/Planet.swift
@@ -46,22 +46,4 @@ struct Planet: Decodable {
     let created: String
     let edited: String
     let url: String
-    
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.name = try container.decode(String.self, forKey: .name)
-        self.rotationPeriod = try container.decode(String.self, forKey: .rotationPeriod)
-        self.orbitalPeriod = try container.decode(String.self, forKey: .orbitalPeriod)
-        self.diameter = try container.decode(String.self, forKey: .diameter)
-        self.climate = try container.decode(String.self, forKey: .climate)
-        self.gravity = try container.decode(String.self, forKey: .gravity)
-        self.terrain = try container.decode(String.self, forKey: .terrain)
-        self.surfaceWater = try container.decode(String.self, forKey: .surfaceWater)
-        self.population = try container.decode(String.self, forKey: .population)
-        self.residents = try container.decode([String].self, forKey: .residents)
-        self.films = try container.decode([String].self, forKey: .films)
-        self.created = try container.decode(String.self, forKey: .created)
-        self.edited = try container.decode(String.self, forKey: .edited)
-        self.url = try container.decode(String.self, forKey: .url)
-    }
 }
--- StarWars/ServerModels/Starship.swift
+++ StarWars/ServerModels/Starship.swift
@@ -54,26 +54,4 @@ struct Starship: Decodable {
     let created: String
     let edited: String
     let url: String
-    
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.name = try container.decode(String.self, forKey: .name)
-        self.model = try container.decode(String.self, forKey: .model)
-        self.manufacturer = try container.decode(String.self, forKey: .manufacturer)
-        self.costInCredits = try container.decode(String.self, forKey: .costInCredits)
-        self.length = try container.decode(String.self, forKey: .length)
-        self.maxAtmospheringSpeed = try container.decode(String.self, forKey: .maxAtmospheringSpeed)
-        self.crew = try container.decode(String.self, forKey: .crew)
-        self.passengers = try container.decode(String.self, forKey: .passengers)
-        self.cargoCapacity = try container.decode(String.self, forKey: .cargoCapacity)
-        self.consumables = try container.decode(String.self, forKey: .consumables)
-        self.hyperdriveRating = try container.decode(String.self, forKey: .hyperdriveRating)
-        self.MGLT = try container.decode(String.self, forKey: .MGLT)
-        self.starshipClass = try container.decode(String.self, forKey: .starshipClass)
-        self.pilots = try container.decode([String].self, forKey: .pilots)
-        self.films = try container.decode([String].self, forKey: .films)
-        self.created = try container.decode(String.self, forKey: .created)
-        self.edited = try container.decode(String.self, forKey: .edited)
-        self.url = try container.decode(String.self, forKey: .url)
-    }
 }
